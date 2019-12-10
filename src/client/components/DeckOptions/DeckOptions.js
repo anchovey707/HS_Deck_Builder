24ea372d45baab89ed44900c78d73d1c5f87e51f
Eric Dearing <ericdearing24@yahoo.com>
2019-12-10 14:37:38 -0500

    Added buttons to deck options field

--- src/client/components/DeckOptions/DeckOptions.js
+++ src/client/components/DeckOptions/DeckOptions.js
@@ -2,16 +2,24 @@ import React from 'react';
 
 const DeckOptions = () => (
   <div className='deck-options' style={styles.DeckOptions}>
-    Deck Options
+    Deck Options 
+   <br></br>
+  <button >Load</button>
+  <button>Save</button>
+  <button>Filter</button>
+  <button>Delete</button>
+ 
   </div>
-)
+);
 
 const styles = {
   DeckOptions:{
     height: '10vh',
     width: '22vw',
     border:'1px solid black',
-  }
+    
+  },
+ 
 }
 
 export default DeckOptions;
