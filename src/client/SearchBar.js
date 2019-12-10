1d3bd5580076ceb29a212fce3ebba46ced4699d3
NiazAhmed151 <niaz151@gmail.com>
2019-12-10 11:47:25 -0500

    Added Input Field to Search Bar

--- src/client/SearchBar.js
+++ src/client/SearchBar.js
@@ -4,6 +4,9 @@ import { FaSearch } from 'react-icons/fa';
 
 const SearchBar = () => (
   <div className='search-bar' style={styles.SearchBar}>
+    <form>
+      <input type='text' style={styles.FormInput} />
+    </form>
     <span className='search-icon' style={styles.SearchIcon}>
       <FaSearch/>
     </span>
@@ -20,9 +23,19 @@ const styles = {
     alignItems:'center',
     position: 'relative'
   },
+  FormInput:{
+    height: '4vh',
+    width: '18vw',
+    marginLeft:'1vw',
+    borderRadius: '20px',
+    outline: 'none',
+    border: 'none',
+    fontSize: '12px'
+
+  },
   SearchIcon:{
     position:'absolute',
-    right: 20
+    right: 15
   }
 }
 
