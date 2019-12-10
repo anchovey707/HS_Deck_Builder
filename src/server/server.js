b473ae6f82304bfddcd17299bb88edf21c13f51c
Anthony Shedlowski <anchovey707@yahoo.com>
2019-12-10 16:00:00 -0500

    Added a basic statement for the retrieve deck GET request

--- src/server/server.js
+++ src/server/server.js
@@ -2,10 +2,25 @@ var http = require('http');
 
 var server = http.createServer((req, res) => {
     console.log("Method: " + req.method);
-    console.log("URL: " + req.url);
+    console.log("URL: '" + req.url+"'");
     
     if(req.method === "GET") {
         res.write("Response");
+
+        //if trying to retrieve deck
+        if(/^\/getDeck\?/g.test(req.url)){
+
+            let params=req.url.substring(9).split('&');
+
+            for(let p=0;p<params.length;p++){
+                params[p]=params[p].split('=')[1];
+                console.log("  param "+p+"="+params[p]);
+            }
+            
+            //sql.getDeck(param[0],param[1])
+            res.write("Here's that deck");
+            
+        }
     }
     res.end();
 
