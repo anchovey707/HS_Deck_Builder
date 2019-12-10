7ce2b04cb86f78a2bab07dcf45a0e9c920b29c6a
Anthony Shedlowski <anchovey707@yahoo.com>
2019-12-10 15:29:07 -0500

    DB script and readme info

---
+++ HS_DB.sql
@@ -0,0 +1,35 @@
+DROP SCHEMA IF EXISTS `hs_decks`;
+
+CREATE SCHEMA IF NOT EXISTS `hs_decks` DEFAULT CHARACTER SET utf8;
+USE `hs_decks`;
+
+DROP user hs_user@'%';
+FLUSH PRIVILEGES;
+CREATE USER hs_user@'%' IDENTIFIED BY 'hearthstone';
+GRANT ALL PRIVILEGES ON hs_decks.* TO hs_user@'%';
+FLUSH PRIVILEGES;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+insert into users values(1,'test','test');
+
+CREATE TABLE IF NOT EXISTS `decks` (
+  `ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `userID` INT(10) UNSIGNED NOT NULL,
+  `name` VARCHAR(64) NOT NULL,
+  `cardData` VARCHAR(256) NOT NULL,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `user_id`
+    FOREIGN KEY (`userID`)
+    REFERENCES `hs_decks`.`users` (`ID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 
 var server = http.createServer((req, res) => {
+    console.log("Method:"+req.method);
+    console.log("URL:"+req.url);
     res.write("Hello");
     res.end();
 });
