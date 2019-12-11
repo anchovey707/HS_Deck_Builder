f83ad5124b70b524c039796c2fcc9d643a3263f2
NiazAhmed151 <niaz151@gmail.com>
2019-12-11 10:40:05 -0500

    Added Page Count Reducer

---
+++ src/client/redux/reducers/PageCountReducer.js
@@ -0,0 +1,18 @@
+var initial_state = 1
+
+export default (state = initial_state, action) => {
+  switch(action.type){
+
+    case 'INCREASE_PAGE_COUNT':
+      let next_page = state + 1
+      return next_page
+    
+    case 'DECREASE_PAGE_COUNT':
+      let prev_page = state - 1
+      return prev_page
+
+    default:
+      return state
+
+  }
+}
--- src/client/redux/reducers/index.js
+++ src/client/redux/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import ClickTabReducer from './ClickTabReducer';
 import CardDataReducer from './CardDataReducer';
 import CardPageReducer from './CardPageReducer';
+import PageCountReducer from './PageCountReducer';
 
 export default combineReducers({
   CardTabs:ClickTabReducer,
   CardData: CardDataReducer,
-  CardPages: CardPageReducer
+  CardPages: CardPageReducer,
+  PageCount: PageCountReducer
 });
