b94f123c9c322846c06c1df6b9d97c830fe2626d
gadachikriege <grahamadachikriege@gmail.com>
2019-12-11 10:45:04 -0500

    module exports in sql.js ready for testing

--- src/server/sql.js
+++ src/server/sql.js
@@ -3,54 +3,55 @@ module.exports.getDeck = getDeck;
 module.exports.getUserDecks = getUserDecks;
 module.exports.registerUser = registerUser;
 
-var con = mysql.createConnection({
-  host: "18.234.74.106", 
-  user: "hs_user",
-  password: "hearthstone",
-  database: "hs_decks"
-});
+function runQuery(sqlString){
+    var con = mysql.createConnection({
+        host: "18.234.74.106", 
+        user: "hs_user",
+        password: "hearthstone",
+        database: "hs_decks"
+      });
+      
+      con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+      });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
+    con.query(sqlString, function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+}
 
 
 function getDeck(deckID){
-    sqlQuery = 'SELECT cardData FROM decks \
-                WHERE ID=\'' + deckID + '\';'; //consider adding LIMIT 1 to return inner JSON
-    return sqlQuery;
+    var sqlQuery = 'SELECT cardData FROM decks \
+                    WHERE ID=\'' + deckID + '\';'; //consider adding LIMIT 1 to return inner JSON
+    var result = runQuery(sqlQuery);
+    return result;
 }
 
 function getUserDecks(userID){
-    sqlQuery = 'SELECT ID FROM decks \
-                WHERE userID=\'' + userID + '\';';
-    return sqlQuery;
+    var sqlQuery = 'SELECT ID FROM decks \
+                    WHERE userID=\'' + userID + '\';';
+    var result = runQuery(sqlQuery);
+    return result;
 }
 
 function registerUser(username, password){
-    sqlQuery = 'INSERT INTO users (username, password) VALUES (\'' + username + '\', \'' + password + '\');'
-    return sqlQuery;
+    var sqlQuery = 'INSERT INTO users (username, password) \
+                    VALUES (\'' + username + '\', \'' + password + '\');'
+    var result = runQuery(sqlQuery);
 }
 
 function verifyUser(params){
-
 }
 
 function deleteDeck(params){
-
 }
 
 function saveDeck(params){
-    
 } 
 
 
 
-
-var sql = registerUser('avocado', 'toast');
-con.query(sql, function (err, result) {
-  if (err) throw err;
-  console.log(result);
-});
+registerUser('maple', 'syrup');
