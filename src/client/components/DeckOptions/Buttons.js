2f0cf4b643e7ee21f48f87d0e5a4e9d9d51bae0c
Eric Dearing <ericdearing24@yahoo.com>
2019-12-12 09:25:13 -0500

    added request function to buttons

--- src/client/components/DeckOptions/Buttons.js
+++ src/client/components/DeckOptions/Buttons.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import $ from 'jquery';
 
 class Buttons extends React.Component {
 
+    
 
+    
 
+    testing () {
+        $.ajax ({
+            url: '/registerUser' ,
+            data : {username:'',password:''},
+            type: 'POST',
+        
+            success: function(response) {
+                 console.log(response)
+            },
+        
+           error: function(response ) {
+               console.log(response)
+            }
+        })
+    }
 render () {
 
     return (
@@ -11,7 +29,7 @@ render () {
         <div className = "deckbuttons"  style ={styles.Buttons} >
 
         <button id ="" style={styles.LoadButton}>Load</button>
-        <button id = "" style={styles.SaveButton}>Save</button>
+        <button id = "" onClick={this.testing} style={styles.SaveButton}>Save</button>
         <button id = "" style={styles.DeleteButton}>Delete</button>
         </div>
     );
