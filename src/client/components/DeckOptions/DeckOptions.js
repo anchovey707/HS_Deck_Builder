6feee8384840569d0da33665d37511dc2f067242
Eric Dearing <ericdearing24@yahoo.com>
2019-12-12 12:38:49 -0500

    if this breaks then delete

--- src/client/components/DeckOptions/DeckOptions.js
+++ src/client/components/DeckOptions/DeckOptions.js
@@ -11,21 +11,4 @@ const DeckOptions = () => (
   </div>
 );
 
-<<<<<<< HEAD
-const styles = {
-  DeckOptions:{
-    height: '10vh',
-    width: '22vw',
-    border:'1px solid black',
-    fontSize: '15px',
-    fontWeight: 'bold',
-    textAlign: 'center'
-    
-    
-  },
- 
-}
-
-=======
->>>>>>> 4c0fe43e751a004868e26df5ad30f5b5322a266f
 export default DeckOptions;
--- src/client/components/FilterBar/FilterBar.js
+++ src/client/components/FilterBar/FilterBar.js
@@ -1,22 +1,20 @@
 import React from 'react'
-import '../../../stylesheets/FilterBar.css';
 
 const FilterBar = () => (
-  <div className='filter-bar'>
+  <div className='filter-bar' style={styles.FilterBar}>
     Filter Bar
   </div>
 )
 
-<<<<<<< HEAD
+
 const styles = {
   FilterBar:{
     height: '10vh',
     border:'1px solid black',
     fontWeight: 'bold',
-    textAlign: 'center'
+    textAlign: 'center',
+    
   }
 }
 
-=======
->>>>>>> 4c0fe43e751a004868e26df5ad30f5b5322a266f
 export default FilterBar
